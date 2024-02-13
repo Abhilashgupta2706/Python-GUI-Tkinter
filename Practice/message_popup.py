84973fef08e2ba8e6a89e10ada62bd20cf6fbece
Abhilash Gupta <abhilash.gupta.official@gmail.com>
2024-02-13 13:05:19 +0530

    Enter Key Press & Confirmation DialogBox

--- Practice/message_popup.py
+++ Practice/message_popup.py
@@ -7,12 +7,15 @@ class MyGUI:
 
     def __init__(self):
         self.root = tk.Tk()
+        self.root.title("Message Popup")
         self.root.geometry("500x300")
 
-        self.label = tk.Label(self.root, text="Your Message", font=("Arial", 18))
+        self.label = tk.Label(self.root, text="Write your message", font=("Arial", 18))
         self.label.pack(padx=10, pady=10)
 
         self.textbox = tk.Text(self.root, height=3, font=("Arial", 18))
+        # Binding a KeyPress and pass it ti a new function | It automatically passes a event to that function
+        self.textbox.bind("<KeyPress>", self.shortcut)
         self.textbox.pack(padx=10, pady=10)
 
         self.check_state = tk.IntVar()  # To store the checkbox value
@@ -31,6 +34,8 @@ class MyGUI:
 
         self.button.pack(padx=10, pady=10)
 
+        # "WM_DELETE_WINDOW":  tell that user is clicking on close "X" symbol
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.mainloop()
 
     def show_message(self):
@@ -41,9 +46,20 @@ class MyGUI:
         )  # Get the text from textbox
 
         if state_of_checkbox:
-            messagebox.showinfo(title="Message", message=text_from_textbox)
+            if len(text_from_textbox) != 1:
+                messagebox.showinfo(title="Message", message=text_from_textbox)
+
         else:
             print(f"No message box should be displayed. Here is your message {text_from_textbox}")
+
+    def shortcut(self, event):  # Event will have details about the key pressed
+        if event.state in (4, 6) and event.keysym == 'Return':
+            self.show_message()
+
+    def on_closing(self):
+        # Confirmation dialog box
+        if messagebox.askyesno(title="Close windows?", message="Do you really want to close this windows?"):
+            self.root.destroy()  # To destroy the Tkinter
 
 
 MyGUI()
